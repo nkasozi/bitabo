ad46cb487a3aa4f59ec03bf3968aa681f927f441
Nkasozi <nkasozi@gmail.com>
2025-03-03 05:41:12 +0300

    fix broken build

---
+++ src/routes/+layout.ts
@@ -0,0 +1,11 @@
+// Define which routes should be prerendered
+export const prerender = true;
+
+// Allow SSR for all routes
+export const ssr = true;
+
+// Enable client-side router
+export const csr = true;
+
+// Enable trailingSlash handling for all routes
+export const trailingSlash = 'never';
