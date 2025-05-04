440ff3ef8255e531138e8f698ea681c52ae5640c
Nkasozi <nkasozi@gmail.com>
2025-05-04 05:22:35 +0300

    send console logs to sentry

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,4 +1,4 @@
-import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import { captureConsoleIntegration, handleErrorWithSentry, replayIntegration } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
@@ -15,7 +15,7 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // If you don't want to use Session Replay, just remove the line below:
-  integrations: [replayIntegration()],
+  integrations: [captureConsoleIntegration()],
 });
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
